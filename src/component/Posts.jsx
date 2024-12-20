d5d2b5b4ba09d2f440589cea765e836bbba36873
shayankhannn <shayannkhannn007@gmail.com>
2024-12-20 18:57:14 +0500

    #

--- src/component/Posts.jsx
+++ src/component/Posts.jsx
@@ -1,33 +1,34 @@
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { fetchData } from "../app/features/crud/CrudSlice"
-import PostsCard from "./PostsCard"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "../app/features/crud/CrudSlice";
+import PostsCard from "./PostsCard";
 
 const Posts = () => {
+  const Dispatch = useDispatch();
+  const Posts = useSelector((state) => state.posts.data);
+  const status = useSelector((state) => state.posts.status);
 
-    const Dispatch = useDispatch()
-    const Posts = useSelector((state) => state.posts.data)
-    const status = useSelector((state) => state.posts.status)
-
-
-    useEffect(()=>{
-    if(status === 'idle'){
-        Dispatch(fetchData())
+  useEffect(() => {
+    if (status === "idle") {
+      Dispatch(fetchData());
     }
-   
-},[Dispatch,status])
+  }, [Dispatch, status]);
 
   return (
-    
-    <div >
-    {status === 'loading' && <h1>Loading...</h1>}
-    {status === 'succeeded' && ( Posts.map((post)=>
-     <div className="flex flex-wrap justify-center gap-6">
-    <PostsCard key={post.id} title={post.title} body={post.body} />
-    </div>
-   ) )}
-    </div>
-  )
-}
+    <div>
+      <button className="px-4 py-2 text-sm text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
+        Add Post
+      </button>
 
-export default Posts
+      {status === "loading" && <h1>Loading...</h1>}
+      {status === "succeeded" &&
+        Posts.map((post) => (
+          <div className="flex flex-wrap justify-center gap-2">
+            <PostsCard key={post.id} title={post.title} body={post.body} />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Posts;
